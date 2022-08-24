3624e144e4f17e7b42ee4905f9a00d89efb1b197
Christian Eliopoulos <cge7497@rit.edu>
2022-08-24 14:28:07 -0400

    Installs okta packages, sets up demo functionality
    Okta was implemented following https://developer.okta.com/docs/guides/sign-into-spa-redirect/angular/main/

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,15 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { OktaAuthModule, OKTA_CONFIG } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+import { ProfileComponent } from './profile/profile.component';
+
+import {config} from "../config/app.config";
+
+// This page may be helpful for getting these values: https://developer.okta.com/docs/guides/sign-into-spa-redirect/angular/main/#find-your-config-values
+// This page is helpful for future work: https://developer.okta.com/docs/guides/sign-into-spa-redirect/angular/main/#sign-in-a-user
+const oktaAuth = new OktaAuth({
+  issuer: config.issuer,
+  clientId: config.clientId,
+  redirectUri: window.location.origin + '/login/callback'
+});
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    OktaAuthModule
   ],
-  providers: [],
+  providers: [{ provide: OKTA_CONFIG, useValue: { oktaAuth } }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ angular/src/config/app.config_empty.ts
@@ -0,0 +1,6 @@
+// Put your okta details, and then rename this file to app.config
+
+export const config = {
+    issuer: '',
+    clientId: ''
+  };
