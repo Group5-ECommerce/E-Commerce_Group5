b5d7f601b3eef91889b3d13ee77cf12407162e94
Christian Eliopoulos <cge7497@rit.edu>
2022-08-30 16:54:07 -0400

    Sets up changePassword values
    Makes sure new password is equal to confirmation, and checks for null textboxes

--- angular/src/app/change-password/change-password.component.ts
+++ angular/src/app/change-password/change-password.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./change-password.component.css']
 })
 export class ChangePasswordComponent implements OnInit {
-
-  constructor() { }
-
   ngOnInit(): void {
+  }
+
+  changePassword(e: Event) {
+    e.preventDefault();
+    let oldPass = (<HTMLInputElement>document.querySelector("#changePasswordForm #oldPass")).value;
+    let newPass = (<HTMLInputElement>document.querySelector("#changePasswordForm #newPass")).value;
+    let newPass2 = (<HTMLInputElement>document.querySelector("#changePasswordForm #newPass2")).value;
+
+    if (!newPass || !newPass2 || !oldPass){
+      alert("Please insert values for all textboxes.");
+      return;
+    }
+    if ( newPass !== newPass2){
+      alert("The two password do not match!");
+      return;
+    }
+
+    let profile:Object = new Object();
+    if (oldPass) Object.assign(profile, {'oldPassword': oldPass});
+    if (newPass) Object.assign(profile, {'newPassword': newPass});
+
+    // const url = config.issuer + "/api/v2/users/" + this.userId;
+    // console.log(url);
+    //this.http.post(url, profile);
   }
 
 }
