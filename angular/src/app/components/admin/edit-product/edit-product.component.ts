8a4efad954cdc88a159a10d0629257c1c4c34bfb
Gaurav-Shinde <gaurav.shinde@gmail.com>
2022-09-01 12:35:08 -0400

    edit product submission bug fix

--- angular/src/app/components/admin/edit-product/edit-product.component.ts
+++ angular/src/app/components/admin/edit-product/edit-product.component.ts
@@ -14,7 +14,8 @@ export class EditProductComponent implements OnInit {
   id!: number;
   product = new Product();
   showAlert = false;
-  file?: File
+  file: File | undefined
+  useExistingImg?: boolean
 
   ngOnInit() {
     this.id = this.route.snapshot.params.id;
@@ -22,32 +23,53 @@ export class EditProductComponent implements OnInit {
     this.productService.getProduct(this.id).subscribe((response: any) => {
       this.product = response;
     })
+
+    this.useExistingImg = false
   }
 
-  async updateProduct() {
-    await this.uploadImgToCloud()
-    this.productService.updateProduct(this.product).subscribe((response: any) => {
-      console.log(response);
-      this.product = new Product();
-      this.showAlert = true;
-    })
+  // async updateProduct() {
+  //   await this.uploadImgToCloud().catch((err) => console.log(err))
+
+  //   //still allows this part execution after err
+  //   this.productService.updateProduct(this.product).subscribe((response: any) => {
+  //     console.log(response);
+  //     this.product = new Product();
+  //     this.showAlert = true;
+  //   })
+  // }
+  updateProduct() {
+    this.uploadImgToCloud().then(
+      (result) => {
+        this.productService.updateProduct(this.product).subscribe((response: any) => {
+          this.product = new Product();
+          this.showAlert = true;
+        })
+      },
+      (error) => { console.log(error) }
+    )
+
   }
 
   setFile(event: any) {
     this.file = event.target.files[0];
-    console.log(this.file)
   }
 
   uploadImgToCloud() {
     return new Promise<string>((resolve, reject) => {
-
+      //has to be else-if or reject statement fires despite returning from resolve
       if (this.file) {
         this.productService.saveImgToCloudinary(this.file).subscribe((response: any) => {
           console.log(response)
           this.product.productImage = response.secure_url;
           resolve("image cloud api url saved")
         })
-      } else { reject('img file null or undefined') }
+      }
+      else if (this.file === undefined && this.useExistingImg) {
+        resolve('use existing img for edit')
+      } else {
+        reject('img file null and useExisingImg is false')
+      }
+
 
     })
   }
