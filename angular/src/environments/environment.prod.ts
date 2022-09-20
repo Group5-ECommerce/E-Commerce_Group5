e45bb27bcae7ce76983ca4003187f01db53e095f
hclrsassine <109600928+hclrsassine@users.noreply.github.com>
2022-09-20 12:34:01 -0400

    update environment.prod

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  stripePublishableKey: "pk_test_51LiLnEH4jfIC0NAIoZefd4iPzn8fBxQDTHtqwIng6mjZp0OExRBYbCZFU2V9XgHmD6BGpWga6Q0u6uPr9kWUBl9l000AZGoBE2",
+  appInsights: {
+    instrumentationKey:'33da3c0d-6fef-4dac-93f8-aab1706923db'
+  }
 };
