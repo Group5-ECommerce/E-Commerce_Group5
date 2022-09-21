d5307fcb7a0c1e17798231f486e62dd7b45c4720
hclrsassine <109600928+hclrsassine@users.noreply.github.com>
2022-09-21 13:03:39 -0400

    added backend url to environment

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
   stripePublishableKey: "pk_test_51LiLnEH4jfIC0NAIoZefd4iPzn8fBxQDTHtqwIng6mjZp0OExRBYbCZFU2V9XgHmD6BGpWga6Q0u6uPr9kWUBl9l000AZGoBE2",
   appInsights: {
     instrumentationKey:'33da3c0d-6fef-4dac-93f8-aab1706923db'
-  }
+  },
+  ecommercecapstoneUrl:'https://ecommerce-capstone.azurewebsites.net'
 };
