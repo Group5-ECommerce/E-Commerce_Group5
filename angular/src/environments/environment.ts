cd5c99beb4f581fb320a0a23861e241c638526a2
Christian Eliopoulos <cge7497@rit.edu>
2022-09-22 14:55:08 -0400

    Updates stripe

--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  stripePublishableKey: "pk_test_51LiLnEH4jfIC0NAIoZefd4iPzn8fBxQDTHtqwIng6mjZp0OExRBYbCZFU2V9XgHmD6BGpWga6Q0u6uPr9kWUBl9l000AZGoBE2",
+  stripePublishableKey: "pk_test_51LigrpISwnjVGPNFWdz8YkBggDyzOp96R2ReTUMICS4ymBBNzovTan0tG1Ni2l0wfEy5J6Bls3VHKrywejIbQHhB00lNcUwTbg",
   appInsights: {
     instrumentationKey:'33da3c0d-6fef-4dac-93f8-aab1706923db'
   },
