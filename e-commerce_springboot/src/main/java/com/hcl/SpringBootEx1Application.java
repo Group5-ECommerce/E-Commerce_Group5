9899b36893dd07083a3ec99be267d318ab46983d
hclrsassine <109600928+hclrsassine@users.noreply.github.com>
2022-09-26 14:49:12 -0400

    updated appinsights key and method

--- e-commerce_springboot/src/main/java/com/hcl/SpringBootEx1Application.java
+++ e-commerce_springboot/src/main/java/com/hcl/SpringBootEx1Application.java
@@ -3,9 +3,12 @@ package com.hcl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
+
 @SpringBootApplication
 public class SpringBootEx1Application {
     public static void main(String[] args) {
+    	ApplicationInsights.attach();
 		SpringApplication.run(SpringBootEx1Application.class, args);
     }
 }
