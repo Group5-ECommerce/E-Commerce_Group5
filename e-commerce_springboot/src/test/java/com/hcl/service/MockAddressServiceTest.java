8c885720c06659502afd4f0bbd7735106a9e9738
Christian Eliopoulos <cge7497@rit.edu>
2022-10-06 12:14:50 -0400

    Renames file so Maven tests it, updates address service tests
    Maven looks for files named with Test when maven test is ran.
    Before, the address service test file didn't have test in the name, so maven wasn't running it.

--- e-commerce_springboot/src/test/java/com/hcl/service/MockAddressServiceTest.java
+++ e-commerce_springboot/src/test/java/com/hcl/service/MockAddressServiceTest.java
@@ -17,16 +17,21 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.hcl.entity.Address;
+import com.hcl.entity.OrderAddress;
 import com.hcl.repo.AddressRepository;
+import com.hcl.repo.OrderAddressRepository;
 
 @ExtendWith(MockitoExtension.class)
-public class MockAddressService {
+public class MockAddressServiceTest {
 
 	@InjectMocks
 	AddressService addressService;
 
 	@Mock
 	AddressRepository addressRepository;
+	
+	@Mock
+	OrderAddressRepository orderAddressRepository;
 
 	@BeforeEach
 	public void init() {
@@ -44,6 +49,12 @@ public class MockAddressService {
 		Address newAddress = addressService.addAddress("okta1", address);
 		Assertions.assertThat(newAddress).extracting(Address::getOktaId).isEqualTo(address.getOktaId());
 		verify(addressRepository, times(1)).save(address);
+		
+		OrderAddress oa = new OrderAddress();
+		when(orderAddressRepository.save(oa)).thenReturn(oa);
+
+		OrderAddress response = addressService.addOrderAddress("123", oa);
+		Assertions.assertThat(oa.equals(response));
 	}
 
 	// get all addresses
@@ -55,8 +66,27 @@ public class MockAddressService {
 		list.add(new Address());
 		when(addressRepository.findAll()).thenReturn(list);
 		list = addressService.getAllAddress();
+		
 		Assertions.assertThat(list).hasSize(3);
 		verify(addressRepository, times(1)).findAll();
-
+	}
+	
+	@Test
+	void getAddressByIdAndUpdate() {
+		List<Address> list = new ArrayList<Address>();
+		Address a = new Address();
+		a.setState("NY");
+		a.setAddressId(2);
+		list.add(a);
+		list.add(new Address());
+		list.add(new Address());
+		
+		when(addressRepository.findByOktaId("123")).thenReturn(list);
+		list = addressService.getAddressById("123");
+		Assertions.assertThat(list).hasSize(3);
+		// verify(addressRepository, times(1)).findAll();
+		
+		a.setState("PA");
+		addressService.updateAddress(a);
 	}
 }
