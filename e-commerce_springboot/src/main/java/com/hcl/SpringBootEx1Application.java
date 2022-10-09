b38e360ec74feb01b71fc3250e6d2b6313a923c7
Christian Eliopoulos <cge7497@rit.edu>
2022-10-09 13:13:16 -0400

    Revert "Reimplements Rachid's backend app insights"
    This reverts commit 968a1d4f8dba08551654431e94e26832b7d40d40.

--- e-commerce_springboot/src/main/java/com/hcl/SpringBootEx1Application.java
+++ e-commerce_springboot/src/main/java/com/hcl/SpringBootEx1Application.java
@@ -1,15 +1,11 @@
 package com.hcl;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.microsoft.applicationinsights.attach.ApplicationInsights;
 
 @SpringBootApplication
 public class SpringBootEx1Application {
-	
     public static void main(String[] args) {
-    	ApplicationInsights.attach();
 		SpringApplication.run(SpringBootEx1Application.class, args);
     }
 }
